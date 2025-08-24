aafe5f21f7b1f6fc7e293ae2c7573732c42f6719
Koen Van Crombrugge <koenvancrombrugge@hotmail.com>
2025-08-24 21:03:59 +0200

    Change static project data structure to project - issue

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ import ProjectAdd from "./components/ProjectAdd.jsx";
 
 function App() {
   const [projectList, setProjectList] = useState([
-      { id: 1, name: '#143 Fwd: FEBEG formulier', url: 'https://gitlab.com/3sign/clients/febeg/febeg-website-2023/-/issues/143' },
-      { id: 2, name: '#10134 logo OKV op de website bij de partners datum', url: 'https://gitlab.com/3sign/support/-/issues/10134' },
-      { id: 3, name: '#246 Datumformaat weergeven', url: 'https://gitlab.com/3sign/clients/agentschap-justitie-en-handhaving/arrestendatabank/-/issues/246' }
+      { id: 1, name: 'FEBEG', issues: [
+              { id: 1, name: '#143 Fwd: FEBEG formulier', url: 'https://gitlab.com/3sign/clients/febeg/febeg-website-2023/-/issues/143' },
+              { id: 2, name: '#10134 logo OKV op de website bij de partners datum', url: 'https://gitlab.com/3sign/support/-/issues/10134' },
+      ]},
+              { id: 2, name: 'OKV', issues: [
+              { id: 3, name: '#246 Datumformaat weergeven', url: 'https://gitlab.com/3sign/clients/agentschap-justitie-en-handhaving/arrestendatabank/-/issues/246' }
+      ]}
   ]);
 
   function handleUpdateProjectName(targetProject) {
